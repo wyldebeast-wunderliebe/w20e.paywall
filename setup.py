5c02f7804fe605d5d05509f6d64eef63684f9b12
Wietze Helmantel <helmantel@w20e.com>
2017-01-16 20:23:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ version = "1.0.0"
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session='hack')
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
 def read(*rnames):
