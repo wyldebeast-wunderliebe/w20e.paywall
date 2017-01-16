f1f64692d8bfd55ae60bfe4b08c5352e17095b43
Wietze Helmantel <helmantel@wyldebeast-wunderliebe.com>
2017-01-16 23:46:13 +0100

    Moved config into package

--- src/w20e/paywall/__init__.py
+++ src/w20e/paywall/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('settings')
-app.config.from_pyfile('config_overrides.py')
+app.config.from_object('w20e.paywall.settings')
+app.config.from_pyfile('settings_overrides.py')
 app.secret_key = app.config.get('CSRF_SECRET_KEY')
